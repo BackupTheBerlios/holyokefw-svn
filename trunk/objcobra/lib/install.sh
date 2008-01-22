f8f6e2b07d11ce5ed1fefaaeabb94e34f70915c2
citibob <citibob@6ce316f9-3444-0410-91f2-e3f7ae9e1578>
2008-01-22 04:11:27 +0000

    Holyoke Framework --- hacked version of Cobra HTML rendering engine

---
+++ trunk/objcobra/lib/install.sh
@@ -0,0 +1,13 @@
+mvn install:install-file  -Dfile=dom.jar \
+                          -DgroupId=org.citibob.jar \
+                          -DartifactId=dom \
+                          -Dversion=0 \
+                          -Dpackaging=jar \
+                          -DgeneratePom=true
+
+mvn install:install-file  -Dfile=js.jar \
+                          -DgroupId=org.citibob.jar \
+                          -DartifactId=js \
+                          -Dversion=0 \
+                          -Dpackaging=jar \
+                          -DgeneratePom=true
