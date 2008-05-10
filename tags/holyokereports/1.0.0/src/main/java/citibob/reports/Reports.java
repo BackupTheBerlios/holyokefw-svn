5a8587cb787e27efde1288dbb25a6d39f810b558
citibob <citibob@6ce316f9-3444-0410-91f2-e3f7ae9e1578>
2008-05-10 23:02:36 +0000

    Release version 1.0.0

---
+++ tags/holyokereports/1.0.0/src/main/java/citibob/reports/Reports.java
@@ -0,0 +1,396 @@
+/*
+Holyoke Framework: library for GUI-based database applications
+This file Copyright (c) 2006-2008 by Robert Fischer
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+/*
+ * RptConv.java
+ *
+ * Created on October 26, 2007, 8:22 PM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package citibob.reports;
+
+import citibob.app.*;
+import citibob.reports.*;
+import citibob.swing.table.*;
+import javax.swing.*;
+import java.io.*;
+import java.util.*;
+import citibob.text.*;
+import com.Ostermiller.util.*;
+import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.engine.data.*;
+import net.sf.jasperreports.engine.util.*;
+import citibob.sql.*;
+import java.sql.*;
+import javax.swing.table.TableModel;
+
+public abstract class Reports {
+
+///** Used when we need a template just once */
+//protected InputStream openTemplateStream(File dir, String name, int uversionid)
+//throws IOException
+//{
+//	return new FileInputStream(new File(dir, name));
+//}
+
+/** Used when we need a template just once */
+protected byte[] readTemplateFile(File dir, String name, int uversionid)
+throws IOException
+{
+	long size = dir.length();
+	byte[] ret = new byte[(int)size];
+	FileInputStream in = new FileInputStream(new File(dir, name));
+	in.read(ret);
+	in.close();
+	return ret;
+}
+
+/** These must be set by subclass constructor */
+protected String oofficeExe;
+protected citibob.app.App app;
+
+
+// ===================================================================
+public JTypeTableModel toTableModel(java.sql.ResultSet rs)
+throws java.sql.SQLException
+{
+	RSTableModel mod = new citibob.sql.RSTableModel(app.sqlTypeSet());
+	mod.executeQuery(rs);
+//	mod.setRowsAndCols(rs);
+//	mod.setColHeaders(rs);
+//	mod.addAllRows(rs);
+	return mod;
+}
+//public JTypeTableModel toTableModel(SqlRunner str, String sql)
+//{
+//	RSTableModel rsmod = new citibob.sql.RSTableModel(app.getSqlTypeSet());
+//	rsmod.executeQuery(str, sql);
+//	return rsmod;
+//}
+// ===================================================================
+public StringTableModel format(JTypeTableModel mod)
+{
+	return new StringTableModel(mod, app.sFormatMap());
+}
+public StringTableModel format(JTypeTableModel mod, String[] scol, SFormat[] sfmt)
+{
+	return new StringTableModel(mod, app.sFormatMap(), scol, sfmt);
+}
+// ===================================================================
+public JRDataSource toJasper(java.sql.ResultSet rs)
+{
+	return new JRResultSetDataSource(rs);
+}
+public JRDataSource toJasper(JTypeTableModel model)
+{
+	return new JRTableModelDataSource(model);
+}
+public JRDataSource toJasper(java.util.Collection model)
+{
+	return new JRMapCollectionDataSource(model);
+}
+// ===================================================================
+/** @param sgcols Columns to group by.
+ @param sfmap Default conversions between types and strings.
+ @param sgcols Columns to section by at different levels (null if no report sectioning)
+ @param scols Columns for custom formatters (null if none)
+ @param sfmt Custom formatters for those columns
+ @returns A model suitable for JodReports.
+ */
+public List toJodList(JTypeTableModel model,
+String[][] sgcols,
+String[] scols, SFormat[] sfmt)
+{
+	SFormat[] formatters = app.sFormatMap().newSFormats(model, scols, sfmt);
+	StringTableModelGrouper grouper =
+		new StringTableModelGrouper(model, sgcols, formatters);
+	return grouper.groupRowsList();
+}
+public List toJodList(java.sql.ResultSet rs,
+String[][] sgcols,
+String[] scols, SFormat[] sfmt)
+throws SQLException
+{
+	return toJodList(toTableModel(rs), sgcols, scols, sfmt);
+}
+public List toJodList(java.sql.ResultSet rs,
+String[][] sgcols)
+throws SQLException
+{
+	return toJodList(rs, sgcols, null, null);
+}
+public Map toJodMap(JTypeTableModel model,
+String[][] sgcols,
+String[] scols, SFormat[] sfmt)
+{
+	List list = toJodList(model, sgcols, scols, sfmt);
+	Map map = new TreeMap();
+	map.put("g0", list);
+	return map;
+}
+
+/** Converts a set of named Jod Lists to a map. */
+public static Map toJodMap(String[] names, List[] lists)
+{
+	Map map = new TreeMap();
+	for (int i=0; i<names.length; ++i) {
+		map.put(names[i], lists[i]);
+	}
+	return map;
+}
+// ===================================================================
+/** If fout is null, creates a temporary file name to use as output.
+ @param ext filename extension to create (without the dot) */
+File correctFile(String templateName, String ext, File fout)
+throws IOException
+{
+	if (fout != null) return fout;
+
+	// Create a temporary file if needed
+	int dot = templateName.lastIndexOf('.');
+	String outBase = templateName.substring(0, dot);
+	fout = File.createTempFile(outBase, "." + ext);
+	fout.deleteOnExit();
+	return fout;
+}
+
+/** Writes a bunch of JodReports, using template once per item in the list.
+ @param fout File for output; null if we should create a temporary file.
+ @param templateName Name of template to use
+ @param templateDir Directory template file is found in; null if it's in our own jar file
+ @returns Null if no pages generated, otherwise file they're written in. */
+public File writeJodPdfs(List models, File templateDir,
+String templateName, int uversionid, File fout)
+throws IOException, InterruptedException,
+net.sf.jooreports.templates.DocumentTemplateException,
+com.lowagie.text.DocumentException
+{
+	// No data
+	if (models == null) return null;
+	
+	String outExt = "pdf";
+	int dot = templateName.lastIndexOf('.');
+	String inExt = templateName.substring(dot+1);
+
+	fout = correctFile(templateName, outExt, fout);
+	JodPdfWriter jout = new JodPdfWriter(oofficeExe,
+		new FileOutputStream(fout), outExt);
+	try {
+		byte[] template = readTemplateFile(templateDir, templateName, uversionid);
+		for (Iterator ii=models.iterator(); ii.hasNext();) {
+			Map map = (Map)ii.next();
+			InputStream in = new java.io.ByteArrayInputStream(template);
+			System.out.println("Formatting report " + jout.getNumReports());
+			jout.writeReport(in, inExt, map);
+			in.close();
+		}
+	} finally {
+		jout.close();
+	}
+	return (jout.getNumReports() > 0 ? fout : null);
+}
+/** Writes just one JodReport using one template once. */
+public File writeJodPdf(Map map, File templateDir,
+String templateName, int uversionid, File fout)
+throws IOException, InterruptedException,
+net.sf.jooreports.templates.DocumentTemplateException,
+com.lowagie.text.DocumentException
+{
+	// No data
+	if (map == null) return null;
+	
+	List list = new LinkedList();
+	list.add(map);
+	return writeJodPdfs(list, templateDir, templateName, uversionid, fout);
+}
+// ===================================================================
+public void viewJodPdfs(List models, File templateDir,
+String templateName, int uversionid)
+throws IOException, InterruptedException,
+net.sf.jooreports.templates.DocumentTemplateException,
+com.lowagie.text.DocumentException
+{
+	viewPdf(writeJodPdfs(models, templateDir, templateName, uversionid, null));
+}
+public void viewJodPdf(Map map, File templateDir,
+String templateName, int uversionid)
+throws IOException, InterruptedException,
+net.sf.jooreports.templates.DocumentTemplateException,
+com.lowagie.text.DocumentException
+{
+	viewPdf(writeJodPdf(map, templateDir, templateName, uversionid, null));
+}
+// ===================================================================
+public File viewPdf(File file)
+{
+	if (file == null) {
+		javax.swing.JOptionPane.showMessageDialog(null,
+			"The report has no pages.");
+		return null;
+	} else {
+		citibob.gui.BareBonesPdf.view(file);
+		return file;
+	}
+}
+// ===================================================================
+/** @param params extra variables sent to Jasper Report. */
+public void viewJasper(JRDataSource jrdata, Map params, File templateDir,
+String templateName, int uversionid)
+throws JRException, IOException
+{	
+	InputStream reportIn = new ByteArrayInputStream(
+		readTemplateFile(templateDir, templateName, uversionid));
+	try {
+		JasperReport jasperReport = (templateName.endsWith(".jrxml") ?
+			JasperCompileManager.compileReport(reportIn) :
+			(JasperReport)JRLoader.loadObject(reportIn));
+		params = (params == null ? new HashMap() : params);
+		JasperPrint jprint = net.sf.jasperreports.engine.JasperFillManager.fillReport(jasperReport, params, jrdata);
+		JasperPrintersTest.checkAvailablePrinters();		// Java/CUPS/JasperReports bug workaround for Mac OS X
+		net.sf.jasperreports.view.JasperViewer.viewReport(jprint, false);
+	} finally {
+		reportIn.close();
+	}
+}
+public void viewJasper(JRDataSource jrdata, File templateDir,
+String templateName, int uversionid)
+throws JRException, IOException
+{
+	viewJasper(jrdata, new HashMap(), templateDir, templateName, uversionid);
+}
+// ===================================================================
+/** @param reportName Name of report to be used in preferences node pathname.
+ @param ext Filename extension (WITH the dot) to use on report.
+ @param title Title to display in chooser dialog.
+ */
+public File chooseReportOutput(java.awt.Component parent,
+String reportName, String ext, String title)
+//throws Exception
+{
+	java.util.prefs.Preferences pref = app.userRoot().node("reports");
+	final String dotExt = ext;
+	final String starExt = "*" + ext;
+	String dir = pref.get(reportName, null);
+	JFileChooser chooser = new JFileChooser(dir);
+	chooser.setDialogTitle("Save " + title);
+	chooser.addChoosableFileFilter(
+		new javax.swing.filechooser.FileFilter() {
+		public boolean accept(File file) {
+			String filename = file.getName();
+			return filename.endsWith(dotExt);
+		}
+		public String getDescription() {
+			return starExt;
+		}
+	});
+	String path = null;
+	File file;
+	for (;;) {
+		chooser.showSaveDialog(parent);
+
+		path = chooser.getCurrentDirectory().getAbsolutePath();
+		if (chooser.getSelectedFile() == null) return null;
+		String fname = chooser.getSelectedFile().getPath();
+		if (!fname.endsWith(dotExt)) fname = fname + dotExt;
+		file = new File(fname);
+		if (!file.exists()) break;
+		if (JOptionPane.showConfirmDialog(
+			parent, "The file " + file.getName() + " already exists.\nWould you like to ovewrite it?",
+			"Overwrite File?", JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION) break;
+	}
+	pref.put("saveReportDir", path);
+	return file;
+}
+
+public void writeCSV(StringTableModel model,
+java.awt.Component parent,
+String title)
+//String reportName)//, String ext, String title)
+throws IOException, java.text.ParseException
+{
+	File f = chooseReportOutput(parent, "csvreport", ".csv", title);
+	if (f == null) return;
+	writeCSV(model, f);
+}
+	/** Creates a new instance of CSVReportOutput */
+public void writeCSV(StringTableModel model, File f) throws IOException, java.text.ParseException
+{
+	FileWriter out = null;
+	try {
+		out = new FileWriter(f);
+		writeCSV(model, out);
+	} finally {
+		try { out.close(); } catch(Exception e) {}
+	}
+}
+
+/** Creates a new instance of CSVReportOutput */
+public void writeCSV(StringTableModel model, Writer out) throws IOException, java.text.ParseException
+{
+	int ncol = model.getColumnCount();
+
+	CSVPrinter pout = new com.Ostermiller.util.CSVPrinter(out);
+	for (int i=0; i<ncol; ++i) {
+		pout.print(model.getColumnName(i));
+	}
+	pout.println();	
+	
+	// Do each row
+	for (int j=0; j<model.getRowCount(); ++j) {
+		for (int i=0; i<ncol; ++i) {
+			String s = (String)model.getValueAt(j,i);
+			pout.print(s);
+		}
+		pout.println();
+	}
+	pout.flush();
+}
+// ===================================================================
+public File writeXls(Map<String,Object> models,
+File templateDir, String templateName, int uversionid, File fout)
+throws IOException
+{
+	fout = correctFile(templateName, "xls", fout);
+	
+	InputStream reportIn = new ByteArrayInputStream(
+		readTemplateFile(templateDir, templateName, uversionid));
+	PoiXlsWriter poiw = new PoiXlsWriter(reportIn, app.timeZone());
+	poiw.replaceHolders(models);
+	poiw.writeSheet(fout);
+	return fout;
+}
+public File writeXls(TableModel model,
+File templateDir, String templateName, int uversionid, File fout)
+throws IOException
+{
+	Map<String,Object> models = new TreeMap();
+	models.put("rs", model);
+	return writeXls(models, templateDir, templateName, uversionid, fout);
+}
+
+public void viewXls(Map<String,Object> models, File templateDir,
+String templateName, int uversionid)
+throws IOException
+{
+	File f = writeXls(models, templateDir, templateName, uversionid, null);
+	citibob.gui.BareBonesXls.view(f);
+}
+
+}
---
+++ tags/holyokereports/1.0.0/src/main/java/citibob/reports/StringTableModelGrouper.java
@@ -0,0 +1,177 @@
+/*
+Holyoke Framework: library for GUI-based database applications
+This file Copyright (c) 2006-2008 by Robert Fischer
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+/*
+ * TMGrouper.java
+ *
+ * Created on August 11, 2007, 11:02 PM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package citibob.reports;
+
+import citibob.types.JType;
+import citibob.types.JavaJType;
+import java.io.*;
+import java.util.*;
+import citibob.sql.pgsql.*;
+import java.sql.*;
+import citibob.sql.*;
+import citibob.swing.table.*;
+import citibob.swing.typed.*;
+import citibob.text.*;
+
+/**
+ *
+ * @author citibob
+ */
+public class StringTableModelGrouper extends TableModelGrouper
+{
+
+SFormat[] formatters;
+	
+/** Do the grouping, plus a bit of extra packaging for JodReports */
+public Map groupRowsMap()
+{
+	List list = super.groupRowsList();
+	Map map = new TreeMap();
+	map.put("g0", list);
+	return map;
+}
+
+Object newTableModel(int firstrow, int nextrow)
+{
+	JTypeTableModel mod = (JTypeTableModel)super.newTableModel(firstrow, nextrow);
+	return new TemplateTableModel(new StringTableModel(mod, formatters));
+}
+Object formatVal(Object val, int col)
+{
+	try {
+		return formatters[col].valueToString(val);
+	} catch(java.text.ParseException e) {
+		return "x"+val.toString();
+	}
+}
+
+public StringTableModelGrouper(JTypeTableModel model, String[][] sgcols,
+SFormat[] formatters)
+{
+	this(model, getGcols(model, sgcols), formatters);
+}
+public StringTableModelGrouper(JTypeTableModel model, int[][] gcols,
+SFormat[] formatters)
+{
+	super(model, gcols);
+	this.formatters = formatters;
+}
+
+
+
+
+
+
+
+///** Called after groupChanged() for each row. */
+//void processRow() {}
+
+// ----------------------------------------
+// ----------------------------------------
+
+/** Compares for equality, taking null into account */
+private boolean equals(Object a, Object b)
+{
+	if (a == null) return (b == null);
+	if (b == null) return false;
+	return a.equals(b);
+}
+// ====================================================================
+// Test Code
+
+static void print(Map map, int level)
+{
+if (level > 5) return;
+
+	List list = null;
+	for (Iterator<Map.Entry> ii=map.entrySet().iterator(); ii.hasNext(); ) {
+		Map.Entry e = ii.next();
+		Class klass = e.getValue().getClass();
+		if (List.class.isAssignableFrom(klass)) {
+			// sub-list
+			spaces(level);
+			System.out.println(e.getKey() + " = (List)");
+			print((List)e.getValue(), level+1);
+		} else if (JTypeTableModel.class.isAssignableFrom(klass)) {
+			spaces(level);
+			System.out.println(e.getKey() + " = (Table)");
+			print((JTypeTableModel)e.getValue(), level+1);
+		} else if (Map.class.isAssignableFrom(klass)) {
+			spaces(level);
+			System.out.println(e.getKey() + " = (Map)");
+			print((Map)e.getValue(), level+1);
+		} else {
+			spaces(level);
+			System.out.println(e.getKey() + " = " + e.getValue());
+		}
+	}
+	if (list != null) print(list, level+1);
+}
+
+static void print(JTypeTableModel mod, int level)
+{
+	for (int i=0; i<mod.getRowCount(); ++i) {
+		spaces(level);
+		for (int j=0; j<mod.getColumnCount(); ++j) {
+			System.out.print(mod.getValueAt(i,j) + " ");
+		}
+		System.out.println();
+	}
+}
+
+static void print(List list, int level)
+{
+	for (Object o : list) {
+		print((Map)o, level);
+	}
+}
+
+static void spaces(int level)
+{
+	for (int i=0; i<level*2; ++i) System.out.print(" ");
+}
+public static void main(String[] args)
+{
+	JType jString = new JavaJType(String.class);
+	DefaultJTypeTableModel mod = new DefaultJTypeTableModel(new String[][] {
+		{"A", "1", "a"},
+		{"A", "2", "b"},
+		{"A", "2", "c"},
+		{"B", "3", "d"},
+		{"B", "3", "e"},
+		{"B", "4", "f"}
+	}, new String[] {"c0", "c1", "c2"},
+	new JType[] {jString, jString, jString});
+	
+	int[][] gcols = {{0}};
+	TableModelGrouper group = new TableModelGrouper(mod, gcols);
+	
+	print(group.groupRowsList(), 0);
+}
+
+}
+
