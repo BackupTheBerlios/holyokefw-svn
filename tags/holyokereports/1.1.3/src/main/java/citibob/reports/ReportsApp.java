0921574df1c7df634399714a8c48a98788036686
citibob <citibob@6ce316f9-3444-0410-91f2-e3f7ae9e1578>
2008-10-27 05:31:26 +0000

    Release version 1.1.3

---
+++ tags/holyokereports/1.1.3/src/main/java/citibob/reports/ReportsApp.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package citibob.reports;
+
+import citibob.app.App;
+
+/**
+ *
+ * @author citibob
+ */
+public class ReportsApp extends App
+{
+	protected Reports reports;
+	/** Object for report generation */
+	public citibob.reports.Reports reports() { return reports; }
+}
---
+++ tags/holyokereports/1.1.3/svntag.sh
@@ -0,0 +1,1 @@
+svn copy svn+ssh://svn.berlios.de/svnroot/repos/holyokefw/trunk/holyokereports svn+ssh://svn.berlios.de/svnroot/repos/holyokefw/tags/holyokereports/$1 -m "Release version $1"
