ebd6546c2aae990f1d3cc8c2874f60c0623e5930
citibob <citibob@6ce316f9-3444-0410-91f2-e3f7ae9e1578>
2008-12-07 21:11:13 +0000

    Release version 1.1.14

---
+++ tags/holyokefw/1.1.14/src/main/java/citibob/config/StreamSet.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package citibob.config;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ *
+ * @author citibob
+ */
+public interface StreamSet {
+
+public InputStream openStream(String name) throws IOException;
+	
+}
